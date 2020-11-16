61b7923943e1c6096e0793d6aa9373250d2660b7
Long Huynh <Longhuynh741@yahoo.com>
2020-11-16 02:08:27 -0500

    got api search to work

--- src/pages/SearchBooks/index.js
+++ src/pages/SearchBooks/index.js
@@ -1,9 +1,143 @@
 import React from "react";
+import API from "./searchApi";
+import { useState, useEffect } from "react";
+import bookStyle from "./searchBooks.css";
 
 function SearchBooks() {
+  const [search, setSearch] = useState([]);
+  const [userInput, setInput] = useState([]);
+
+  const [type, setType] = useState({
+    title: "",
+    url: "",
+    imageUrl: "",
+  });
+
+  function changeState(e) {
+    setInput(e);
+  }
+
+  useEffect(() => {
+    API.bookSearch("Harry Potter")
+      .then((response) => {
+        setSearch(response.data.items);
+      })
+      .catch((err) => {
+        if (err) throw err;
+      });
+  }, []);
+
+  function handleSearch(event) {
+    event.preventDefault();
+    API.bookSearch(userInput)
+      .then((response) => {
+        setSearch(response.data.items);
+      })
+      .catch((err) => {
+        if (err) throw err;
+      });
+  }
+
+//   function handleFormSubmit(title, url, image) {
+//     console.log(title);
+
+//     setType({
+//       title: title,
+//       url: url,
+//       image: image,
+//     });
+//     API.saveArticle({
+//       title: title,
+//       url: url,
+//       imageUrl: image,
+//     });
+//   }
+
   return (
-<div> </div>
+    <div id="articlesBackground">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-3" />
+          <div className="col-md-6 text-center">
+            <h2 id="articlesHeader">Search Books Here </h2>
+            <>
+              <form>
+                <input
+                  className="form-control nav-search text-center"
+                  id="articlesSearchBar"
+                  type="text"
+                  placeholder="e.g Harry Potter"
+                  name="search"
+                  onChange={(e) => changeState(e.target.value)}
+                  style={{ display: "inline-grid" }}
+                />
+                <button className="articlesBtn" onClick={handleSearch}>
+                  Search
+                </button>
+              </form>
+              <div className="row">
+                {search.map((type) => {
+                  return (
+                    <div key={type.id} style={bookStyle} className="col-sm-4">
+                      <div className="card plant-card">
+                        <div
+                          className="practice image"
+                          style={{
+                            backgroundImage: `url(${type.volumeInfo.imageLinks.smallThumbnail})`,
+                            height: "350px",
+                            width: "100%",
+                            backgroundPosition: "center",
+                            backgroundSize: "cover",
+                            backgroundRepeat: "no-repeat",
+                          }}
+                        ></div>
+
+                        <div className="card-body">
+                          <h5 className="card-title">
+                            <u>{type.common_name}</u>
+                          </h5>
+                          <p className="card-text">
+                            <b>Title:</b> {type.volumeInfo.title}
+                          </p>
+                        </div>
+                        <ul className="list-group list-group-flush">
+                          <li className="list-group-item">
+                            <b>Authors:</b> {type.volumeInfo.authors}
+                          </li>
+                          <li className="list-group-item">
+                            <b>Description:</b> {type.volumeInfo.description}
+                          </li>
+                          <li className="list-group-item">
+                            <b>Link:</b> {type.selfLink}
+                          </li>
+                        </ul>
+                        <div className="card-body">
+                          {/* <button
+                            className="articlesBtn"
+                            onClick={() =>
+                              handleFormSubmit(
+                                type.common_name,
+                                type.image_url,
+                                type.bibliography,
+                                type.family,
+                                type.genus,
+                                type.scientific_name
+                              )
+                            }
+                          >
+                            Save to My Account
+                          </button> */}
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
-
 export default SearchBooks;
---
+++ src/pages/SearchBooks/searchApi.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+// const SEARCHURL = "https://content.guardianapis.com/search?q="
+// const APIKEY = "&show-fields=thumbnail&api-key=d3d603c5-e29e-41d6-abcd-b89ac28211e1";
+
+// eslint-disable-next-line
+export default {
+    bookSearch: function(query) {
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=20`);
+    }
+
+};
